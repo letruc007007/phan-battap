7991e46305a3e1eb0d3c8bb1f104c4460b61eba1
Administrator <Administrator@DEE39>
2024-08-23 15:43:21 +0700

    BaiTap2_KeThua

--- src/main/java/HINHTRON/hinhtron/hinhtru.java
+++ src/main/java/HINHTRON/hinhtron/hinhtru.java
@@ -6,37 +6,8 @@ package HINHTRON.hinhtron;
 
 /**
  *
- * @author ADMIN
+ * @author Administrator
  */
-public class hinhtru extends hinhtron {
-    private double chieucao;
-
-    public hinhtru() {
-        super();
-        this.chieucao = 0.0;
-    }
-
-    public hinhtru(double bankinh, double chieucao) {
-        super(bankinh);
-        this.chieucao = chieucao;
-    }
-
-    public void setChieuCao(double chieucao) {
-        this.chieucao = chieucao;
-    }
-
-    public double getChieuCao() {
-        return this.chieucao;
-    }
-
-    public double tinhTheTich() {
-        return tinhDienTich() * chieucao;
-    }
-
-    @Override
-    public double tinhDienTich() {
-        double s_day=super.tinhDienTich();
-        double s_xq = super.tinhChuVi()*chieucao;
-        return 2*s_day +s_xq;
-    }
+public class hinhtru {
+    
 }
